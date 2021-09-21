60e97c9c622cf6941befc11ad96b95debd9362ac
ahmednoaman12 <ahmednoamanhassan@gmail.com>
2021-09-21 18:15:20 +0200

    pallerx and animation updated

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
+$(function () {
+    'use strict'
 
-// start offcanvas
-(function () {
-  'use strict'
-  
-  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
-  })
+    // start offcanvas
+    document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+        document.querySelector('.offcanvas-collapse').classList.toggle('open')
+    })
+    // end offcanvas
 
-
-      // Back-To-School owl slider
-      $('.Back-To-School .owl-carousel').owlCarousel({
+    // Back-To-School owl slider
+    $('.Back-To-School .owl-carousel').owlCarousel({
         loop: true,
         rtl: false,
-        margin: 20, 
+        margin: 20,
         // center: true,
         nav: true,
         navText: [
             // `<img src='${egy_tempurl}/images/owl_right_arrow.svg' >`,`<img src='${egy_tempurl}/images/owl_left_arrow.svg' >`
             // `<img class ='arrow-circle' src='images/Icon ionic-ios-arrow-back-1.png' >`,`<img class ='arrow-circle' src='images/Icon ionic-ios-arrow-back-1.png' >`
-            `<div class ='arrow-circle   mx-2' ><i class="arrows bi bi-chevron-left"></i> </div>`,`<div class =' mx-2 arrow-circle  ' ><i class="arrows bi bi-chevron-right"></i> </div>`
+            `<div class ='arrow-circle   mx-2' ><i class="arrows bi bi-chevron-left"></i> </div>`, `<div class =' mx-2 arrow-circle  ' ><i class="arrows bi bi-chevron-right"></i> </div>`
         ],
         autoplay: true,
         // autoplayHoverPause: true,
@@ -34,8 +33,35 @@
             }
         }
     });
+    // search
+    $('.header').on('click', '.search-toggle', function (e) {
+        var selector = $(this).data('selector');
 
+        $(selector).toggleClass('show').find('.search-input').focus();
+        $(this).toggleClass('active');
 
-})()
+        e.preventDefault();
+    });
+    // move backgrond
+    $('html').mousemove(function (e) {
 
-// end offcanvas
+        var wx = $(window).width();
+        var wy = $(window).height();
+
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop;
+
+        var newx = x - wx / 2;
+        var newy = y - wy / 2;
+
+        $('.grades .move').each(function () {
+            var speed = $(this).attr('data-speed');
+            if ($(this).attr('data-revert')) speed *= -1;
+            TweenMax.to($(this), 1, {
+                x: (1 - newx * speed),
+                y: (1 - newy * speed)
+            });
+        });
+    });
+
+});
